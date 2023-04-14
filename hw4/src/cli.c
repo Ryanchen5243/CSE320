53f52a7994d7cc1f548d418cc1fe88cdc5e4e4a5
Ryan Chen <ryachen@cs.stonybrook.edu>
2023-04-14 05:39:34 -0400

    second

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -7,6 +7,29 @@
 
 // ajsjajs
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+extern volatile sig_atomic_t user_input_detected;
+extern sigset_t sigSuspendMask;
+extern int quitDetected;
+extern int firstHandle;
+static int watchersCount = 0;
+static WATCHER* watcherTable[100];
+static int cmdInputLen;
+extern volatile sig_atomic_t dataAvailable;
+int numBytesReadGlobal;
+
+
+char* bufferUserInput();
+
+static int handleUserCommand(char*);
+static void insertIntoWatcherTable(WATCHER* w);
+static void displayWatchers();
+static void freeAndRemoveFromWatcherTable(WATCHER* w);
+static void cleanUpWatchers();
+
 
 typedef struct watcher {
     int tableIndex;
@@ -24,28 +47,222 @@ WATCHER *cli_watcher_start(WATCHER_TYPE *type, char *args[]) {
     newCLI->fdin = 0;
     newCLI->fdout = 1;
     newCLI->channel = NULL;
-    write(STDOUT_FILENO,"ticker> ",8);
+    // initialize watcher table
+    for(int i =0; i< 100;i++){
+        watcherTable[i] = NULL;
+    }
+    insertIntoWatcherTable(newCLI);
+    cli_watcher_send(newCLI,"ticker> ");
     return newCLI;
 }
 
 int cli_watcher_stop(WATCHER *wp) {
     // TO BE IMPLEMENTED
-    free(wp);
-
+    kill(wp->pid,SIGKILL);
+    freeAndRemoveFromWatcherTable(wp);
     return 0;
 }
 
 int cli_watcher_send(WATCHER *wp, void *arg) {
-    // TO BE IMPLEMENTED
-    abort();
+    if(strncmp((char*)arg, "ticker> ", strlen("ticker> ")) == 0) { // prompt user input
+        write(STDOUT_FILENO, arg, strlen((char*)arg));
+        fflush(stdout);
+        return 0;
+    }
+    // else check fd for stdout to write data to
+    return 0;
 }
 
+// handling user command
 int cli_watcher_recv(WATCHER *wp, char *txt) {
-    // TO BE IMPLEMENTED
-    abort();
+    // debug("text is ;;;;;; %s",txt);
+    // split txt into parts pass into handle in loop
+    char* cur = txt;
+    // debug("strlennn %zu",strlen(txt));
+    int len = strlen(txt);
+    for(int i = 0; i < len;i++){
+        if(txt[i] == '\n'){
+            txt[i] = '\0';
+            debug("curr %s",cur);
+            handleUserCommand(cur);
+            cur = txt+1+i;
+        }
+    }
+
+    if(quitDetected == 1 || numBytesReadGlobal==0){
+        cleanUpWatchers();
+        return 0;
+    }
+
+    // else ck fd for stdin to read from
+    return 0;
 }
 
 int cli_watcher_trace(WATCHER *wp, int enable) {
     // TO BE IMPLEMENTED
-    abort();
+    return 0;
+}
+
+// read from buffer
+// on success return ptr to buffer, caller to free
+// on failure, frees ptr returns nULL
+char* bufferUserInput() {
+    // stream -> openmemstream -> dynamic buffer(not used)
+    char* z; // disposed buffer
+    size_t bufSize; // size of buffer
+    FILE* stream = open_memstream(&z,&bufSize);
+    // read everything from stdin to stream
+    int numBytesRead = 0;
+    int totalBytesRead = 0;
+    char tempBuf[100];
+    while((numBytesRead = read(STDIN_FILENO,tempBuf,sizeof(tempBuf))) > 0){
+        // debug("Temp Buf is now: %s\n",tempBuf);
+        // write tempBuf to stream
+        int bytesWritten = fwrite(tempBuf,sizeof(char),numBytesRead,stream);
+        if (bytesWritten < numBytesRead) {
+            perror( "Error writing to output\n");
+            fclose(stream);
+            cleanUpWatchers();
+            exit(1);
+        }
+        // debug("in loop tbd %d",totalBytesRead);
+        totalBytesRead+=numBytesRead;
+    }
+    debug("Temp Buf is now: %s\n",tempBuf);
+    if(numBytesRead == -1){
+        if(errno==EWOULDBLOCK){
+            dataAvailable = 0;
+        }else{
+            perror("laksjdfalskdfj");
+            // handle error
+            fclose(stream);
+            cleanUpWatchers();
+            exit(1);
+        }
+    }
+
+    // debug("helllllllll");
+    // debug("Total Bytes Read %d",totalBytesRead);
+    char* buffer = (char*) malloc(totalBytesRead * sizeof(char));
+    if(buffer==NULL) perror("malloc bffer error");
+    // read from stream
+    int c;
+    int index =0;
+    cmdInputLen = 0;
+    while((c = fgetc(stream)) != EOF){
+        buffer[index] = c;
+        index++;
+        cmdInputLen++;
+    }
+    // debug("returned buffer is %s",buffer);
+    fclose(stream);
+    numBytesReadGlobal = numBytesRead;
+    return buffer;
+}
+
+static int handleUserCommand(char* cmd){
+    // debug("handling user input");
+    int numwords = 0;
+    int numSpace = 0;
+    debug("testing command????????? %s\n",cmd);
+    int i = 0;
+    for(char* p = cmd; i<cmdInputLen; p++,i++){
+        // debug("Iterating ...");
+        if(*p == ' '){
+            numSpace++;
+        }
+    }
+    numwords = numSpace+1;
+    char* cmdPtrs[numwords];
+    int ind = 0;
+    cmdPtrs[ind] = cmd;
+    ind++;
+    i = 0;
+    for(char* p = cmd; i<cmdInputLen; p++,i++){
+        if(*p == ' '){
+            *p = '\0';
+            cmdPtrs[ind] = p+1;
+            ind++;
+        }
+    }
+
+    if(strncmp(cmdPtrs[0], "quit", strlen("quit")) == 0){
+        debug("Invokkginn Quit Function");
+        quitDetected = 1;
+        cleanUpWatchers();
+    } else if(strncmp(cmd,"watchers",strlen("watchers"))== 0){
+        debug("Watchers Function");
+        displayWatchers();
+    } else if(strncmp(cmd,"start",strlen("start"))== 0){
+        debug("Start Function");
+        WATCHER* bsw = watcher_types[BITSTAMP_WATCHER_TYPE].start(&watcher_types[BITSTAMP_WATCHER_TYPE],
+        watcher_types[BITSTAMP_WATCHER_TYPE].argv);
+
+        if(bsw);
+
+    } else if(strncmp(cmd,"stop",strlen("stop"))== 0){
+        debug("Stop Function");
+    } else if(strncmp(cmd,"trace",strlen("trace"))== 0){
+
+    } else if(strncmp(cmd,"untrace",strlen("untrace"))== 0){
+
+    } else if(strncmp(cmd,"show",strlen("show"))== 0){
+
+    } else{
+        // invalid cmd
+        if(!firstHandle){
+            printf("???\n");
+            fflush(stdout);
+        }
+    }
+    return 1;
+}
+
+
+static void insertIntoWatcherTable(WATCHER* w){
+    watcherTable[watchersCount] = w;
+    // update pos field
+    w->tableIndex = watchersCount;
+    watchersCount++;
+
+}
+
+static void displayWatchers(){
+    for(int i = 0; i < watchersCount;i++){
+        if(watcherTable[i]->wtype == CLI_WATCHER_TYPE){
+            // char* cliDis =
+            // write(STDOUT_FILENO,"%d\t%s(%d,%d,%d)\n",watcherTable[i]->tableIndex,
+            //     watcher_types[CLI_WATCHER_TYPE].name,watcherTable[i]->pid,watcherTable[i]->fdin,watcherTable[i]->fdout);
+            char cliDis[100];
+            sprintf(cliDis, "%d\t%s(%d,%d,%d)\n", watcherTable[i]->tableIndex, watcher_types[CLI_WATCHER_TYPE].name, watcherTable[i]->pid, watcherTable[i]->fdin, watcherTable[i]->fdout);
+            debug("clidis %s",cliDis);
+            write(STDOUT_FILENO, cliDis, strlen(cliDis));
+        } else {
+            // printf("%s\n", "bitstamp");
+        }
+    }
+    fflush(stdout);
+}
+
+static void freeAndRemoveFromWatcherTable(WATCHER* w){
+    int index = w->tableIndex;
+    if (index >= 0 && index < 100) {
+        for (int i = index + 1; i < watchersCount; i++) {
+            watcherTable[i-1] = watcherTable[i];
+            watcherTable[i-1]->tableIndex = i-1;
+        }
+        watcherTable[watchersCount-1] = NULL;
+        watchersCount--;
+    }
+    free(w);
+}
+
+// remove all watchers from table
+static void cleanUpWatchers(){
+    // debug("Now quitting program");
+    // go through watcher list and free resources
+    // debug("%d",watchersCount);
+    for(int i = 0; i < watchersCount;i++){
+        freeAndRemoveFromWatcherTable(watcherTable[i]);
+    }
 }
--- hw4/src/ticker.c
+++ hw4/src/ticker.c
@@ -19,17 +19,14 @@ typedef struct watcher {
     char* channel;
 } WATCHER;
 
-static int handleUserInput();
-static int handleUserCommand(char*);
-static void insertIntoWatcherTable(WATCHER* w);
-static void displayWatchers();
 volatile sig_atomic_t user_input_detected = 0;
-static WATCHER* watcherTable[100];
-static int watchersCount = 0;
-static void removeFromWatcherTable(WATCHER* w);
-static int quitDetected = 0;
-static void cleanUpWatchers();
-static int firstHandle;
+sigset_t sigSuspendMask;
+int firstHandle;
+int quitDetected = 0;
+char* userInputLine;
+extern char* bufferUserInput();
+volatile sig_atomic_t dataAvailable = 0;
+extern int numBytesReadGlobal;
 
 void sigint_handler(int s){
     // char* msg = "SIGINT Signal Received\n";
@@ -40,15 +37,15 @@ void sigint_handler(int s){
 void sigio_handler(int s, siginfo_t* info,void* context){
     // char* msg = "SIG IO Signal Recieved\n";
     // write(STDOUT_FILENO,msg,24);
-    debug("SIGIO signal");
+    // debug("SIGIO signal received in handler");
     int oldErrno = errno;
     int fd = info->si_fd;
-    // criticla section - block out signal s
-    sigset_t mask;
-    sigset_t oldMask;
-    sigemptyset(&mask);
-    sigaddset(&mask,s);
-    sigprocmask(SIG_BLOCK,&mask,&oldMask);
+    // // criticla section - block out signal s
+    // sigset_t mask;
+    // sigset_t oldMask;
+    // sigemptyset(&mask);
+    // sigaddset(&mask,s);
+    // sigprocmask(SIG_BLOCK,&mask,&oldMask);
     if (fd == STDIN_FILENO) { // user input
         user_input_detected = 1;
         // printf("User input detected!\n");
@@ -56,7 +53,7 @@ void sigio_handler(int s, siginfo_t* info,void* context){
         printf("handling other SIGIO");
     }
     // unblock signals
-    sigprocmask(SIG_SETMASK,&oldMask,NULL);
+    // sigprocmask(SIG_SETMASK,&oldMask,NULL);
 
     errno = oldErrno; // restore errno
 }
@@ -113,37 +110,32 @@ int ticker(void) {
 
     // start cli watcher and add to watcher table
     WATCHER *cliWatcher = watcher_types[CLI_WATCHER_TYPE].start(&watcher_types[CLI_WATCHER_TYPE],NULL);
-    // initialize watcher table with NULLs
-    for(int i =0; i< 100;i++){
-        watcherTable[i] = NULL;
-    }
-
-    // printf("\n---%p\n",cliWatcher);
-    insertIntoWatcherTable(cliWatcher);
-
-    // char* args[] = {"wss://ws.bitstamp.net",NULL};
-
-    sigset_t sigSuspendMask;
     sigemptyset(&sigSuspendMask);
-
     // check stdin if there exists input already? send sigio signal
     firstHandle = 1;
-    handleUserInput();
+    char* userCmd = bufferUserInput();
+    watcher_types[CLI_WATCHER_TYPE].recv(cliWatcher,userCmd);
     firstHandle = 0;
-    if(quitDetected){
+    if(quitDetected == 1 || numBytesReadGlobal==0){
+        free(userCmd);
         return 0;
     }
-    debug("done handling first user input");
+    // debug("done handling first user input");
 
     while(1){ // loop until receipt of any kind of signal
         // body of main loop
         debug("In main loop");
         if (user_input_detected){
-            handleUserInput();
-            if(quitDetected){
+            userCmd = bufferUserInput();
+            watcher_types[CLI_WATCHER_TYPE].recv(cliWatcher,userCmd);
+            if(quitDetected==1){
+                debug("quit detected in mainnnnnnnnnnnnn");
+                free(userCmd);
                 return 0;
             }
-            write(STDOUT_FILENO,"ticker> ",8);
+            // write(STDOUT_FILENO,"ticker> ",8);
+            // debug("heheeeeeeeee");
+            watcher_types[CLI_WATCHER_TYPE].send(cliWatcher,"ticker> ");
 
             sigset_t mask;
             sigset_t oldMask;
@@ -152,8 +144,9 @@ int ticker(void) {
             // critical section
             user_input_detected = 0;
             sigprocmask(SIG_SETMASK,&oldMask,NULL);
+        } else{
+            // debug("some other signal detected");
         }
-        // else if any other signal
 
         debug("waiting for some kind signal");
         sigsuspend(&sigSuspendMask); // suspend execution until receipt of any kind of signal
@@ -163,169 +156,3 @@ int ticker(void) {
     return 0;
 }
 
-static int handleUserInput() {
-    // stream -> openmemstream -> dynamic buffer(not used)
-    char* z; // disposed buffer
-    size_t bufSize; // size of buffer
-    FILE* stream = open_memstream(&z,&bufSize);
-    // read everything from stdin to stream
-    int numBytesRead = 0;
-    int totalBytesRead = 0;
-    char tempBuf[100];
-
-    while((numBytesRead = read(STDIN_FILENO,tempBuf,sizeof(tempBuf))) > 0){
-        // debug("Temp Buf is now: %s\n",tempBuf);
-        // write tempBuf to stream
-        int bytesWritten = fwrite(tempBuf,sizeof(char),numBytesRead,stream);
-        if (bytesWritten < numBytesRead) {
-            perror( "Error writing to output\n");
-            exit(1);
-        }
-        // debug("in loop tbd %d",totalBytesRead);
-        totalBytesRead+=numBytesRead;
-    }
-    // done writing to stream
-    if(numBytesRead==0){
-        quitDetected = 1;
-        cleanUpWatchers();
-        fclose(stream);
-        return 0;
-    }
-    debug("helllllllll");
-    // debug("Total Bytes Read %d",totalBytesRead);
-    char* buffer = (char*) malloc(totalBytesRead * sizeof(char));
-    if(buffer==NULL) perror("malloc bffer error");
-    // read from stream
-    int c;
-    int index =0;
-    while((c = fgetc(stream)) != EOF){
-        buffer[index] = c;
-        index++;
-    }
-
-    // debug("Buffer looks like %s",buffer);
-    handleUserCommand(buffer);
-    if(quitDetected == 1){
-        // debug("deteced quit -- freeing buffer, closing stream");
-        free(buffer);
-        fclose(stream);
-        cleanUpWatchers();
-        return 0;
-    }
-    // reset stuff
-    // debug("handleuserinput -- free buffer,close stream");
-    free(buffer);
-    fclose(stream);
-    return 0;
-}
-
-static int handleUserCommand(char* cmd){
-    // debug("handling user input");
-    int numwords = 0;
-    int numSpace = 0;
-    for(char* p = cmd; *p; p++){
-        if(*p == ' '){
-            numSpace++;
-        }
-    }
-    numwords = numSpace+1;
-    char* cmdPtrs[numwords];
-    int ind = 0;
-    cmdPtrs[ind] = cmd;
-    ind++;
-    for(char* p = cmd; *p; p++){
-        if(*p == ' '){
-            *p = '\0';
-            cmdPtrs[ind] = p+1;
-            ind++;
-        }
-    }
-    // for(int i = 0; i< numwords;i++){
-    //     printf("CMDPTR ::::: %s\n",cmdPtrs[i]);
-    //     fflush(stdout);
-    // }
-
-    // cmdPtrs consists of pointers to cmd args
-    // sizeof(cmdPtrs)/sizeof(char*)
-    if(strncmp(cmdPtrs[0], "quit", strlen("quit")) == 0){
-        debug("Quit Function");
-        quitDetected = 1;
-    } else if(strncmp(cmdPtrs[0],"watchers",strlen("watchers"))== 0){
-        debug("Watchers Function");
-        displayWatchers();
-    } else if(strcmp(cmdPtrs[0],"start")== 0){
-        debug("Start Function");
-        // cmdPtrs[1] -> wtype
-        // cmdPtrs[2] -> channel
-        WATCHER* bsw = watcher_types[BITSTAMP_WATCHER_TYPE].start(&watcher_types[BITSTAMP_WATCHER_TYPE],
-        watcher_types[BITSTAMP_WATCHER_TYPE].argv);
-
-        if(bsw);
-
-    } else if(strcmp(cmdPtrs[0],"stop")== 0){
-        debug("Stop Function");
-
-    } else if(strcmp(cmdPtrs[0],"trace")== 0){
-
-    } else if(strcmp(cmdPtrs[0],"untrace")== 0){
-
-    } else if(strcmp(cmdPtrs[0],"show")== 0){
-
-    } else{
-        // invalid cmd
-        if(!firstHandle){
-            printf("???\n");
-            fflush(stdout);
-        }
-    }
-    return 1;
-}
-
-static void insertIntoWatcherTable(WATCHER* w){
-    watcherTable[watchersCount] = w;
-    // update pos field
-    w->tableIndex = watchersCount;
-    watchersCount++;
-
-}
-
-static void displayWatchers(){
-    for(int i = 0; i < watchersCount;i++){
-        if(watcherTable[i]->wtype == CLI_WATCHER_TYPE){
-            // char* cliDis = 
-            // write(STDOUT_FILENO,"%d\t%s(%d,%d,%d)\n",watcherTable[i]->tableIndex,
-            //     watcher_types[CLI_WATCHER_TYPE].name,watcherTable[i]->pid,watcherTable[i]->fdin,watcherTable[i]->fdout);
-            char cliDis[100];
-            sprintf(cliDis, "%d\t%s(%d,%d,%d)\n", watcherTable[i]->tableIndex, watcher_types[CLI_WATCHER_TYPE].name, watcherTable[i]->pid, watcherTable[i]->fdin, watcherTable[i]->fdout);
-            debug("clidis %s",cliDis);
-            write(STDOUT_FILENO, cliDis, strlen(cliDis));
-        } else {
-            // printf("%s\n", "bitstamp");
-        }
-    }
-    fflush(stdout);
-}
-
-static void removeFromWatcherTable(WATCHER* w){
-    debug("removing watching from table");
-    int index = w->tableIndex;
-    if (index >= 0 && index < 100) {
-        for (int i = index + 1; i < watchersCount; i++) {
-            watcherTable[i-1] = watcherTable[i];
-            watcherTable[i-1]->tableIndex = i-1;
-        }
-        watcherTable[watchersCount-1] = NULL;
-        watchersCount--;
-    }
-    // free resources
-    free(w);
-}
-
-static void cleanUpWatchers(){
-    debug("Now quitting program");
-    // go through watcher list and free resources
-    debug("%d",watchersCount);
-    for(int i = 0; i < watchersCount;i++){
-        removeFromWatcherTable(watcherTable[i]);
-    }
-}
